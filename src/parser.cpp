61084138e3ee0af11ebd8b8c316da6abb094c9fe
0KABE <RIN.OKAB3@GMAIL.COM>
2025-11-04 01:13:08 +0800

    move parser to source file

--- src/parser.cpp
+++ src/parser.cpp
@@ -3,3 +3,37 @@
 //
 
 #include "parser.h"
+
+Entity BasicParseOnce(std::span<const char>& span) {
+  Entity entity{};
+  for (size_t i = 0; i < span.size(); ++i) {
+    if (span[i] == ';') {
+      entity.name = std::string_view(span.first(i));
+      span = span.subspan(i + 1);
+      break;
+    }
+  }
+
+  const bool negative = span.front() == '-';
+  if (negative) {
+    span = span.subspan(1);
+  }
+
+  for (size_t i = 0; i < span.size(); ++i) {
+    if (span[i] == '.') {
+      assert(i + 1 < span.size());
+      entity.temperature = entity.temperature * 10 + span[i + 1] - '0';
+      span = span.subspan(i + 3);
+      break;
+    }
+    entity.temperature = entity.temperature * 10 + span[i] - '0';
+  }
+  if (negative) {
+    entity.temperature = -entity.temperature;
+  }
+  return entity;
+}
+
+Entity ParseOnceV1(std::span<const char>& span) {
+
+}
