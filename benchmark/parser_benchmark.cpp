29e5d7c5043341452d518cf792fba2160d76fb58
0KABE <RIN.OKAB3@GMAIL.COM>
2025-11-05 03:21:47 +0800

    update benchmark BM_ParseOnce_Base

--- benchmark/parser_benchmark.cpp
+++ benchmark/parser_benchmark.cpp
@@ -3,43 +3,34 @@
 //
 
 #include <benchmark/benchmark.h>
+#include <fmt/format.h>
 
 #include <random>
 #include <string>
-#include <vector>
 
 #include "parser.h"
 
 // benchmark for BasicParseOnce with random generated data in runtime
 static void BM_ParseOnce_Base(benchmark::State& state) {
-  std::string data;
   std::random_device rd;
   std::mt19937 gen(rd());
-  std::uniform_int_distribution name_len_dist(1, 100);
   std::uniform_int_distribution char_dist('a', 'z');
   std::uniform_int_distribution temp_int_dist(-99, 99);
   std::uniform_int_distribution temp_float_dist(0, 9);
 
-  const auto lines = state.range(0);
+  constexpr auto kNameLength = 100;
 
-  for (int i = 0; i < lines; ++i) {
-    const int name_len = name_len_dist(gen);
-    for (int j = 0; j < name_len; ++j) {
-      data += char_dist(gen);
-    }
-    data += ';';
-    data += std::to_string(temp_int_dist(gen));
-    data += '.';
-    data += std::to_string(temp_float_dist(gen));
-    data += '\n';
+  std::string name;
+  for (int i = 0; i < kNameLength; ++i) {
+    name += char_dist(gen);
   }
 
+  auto data = fmt::format("{};{}.{}\n", name, temp_int_dist(gen), temp_float_dist(gen));
+  std::span<const char> span(data);
+
   for (auto _ : state) {
-    std::span<const char> span(data);
-    while (!span.empty()) {
-      benchmark::DoNotOptimize(ParseOnce_Base(span));
-    }
+    benchmark::DoNotOptimize(ParseOnce_Base(span));
   }
 }
 
-BENCHMARK(BM_ParseOnce_Base)->RangeMultiplier(10)->Range(10, 1e6);
+BENCHMARK(BM_ParseOnce_Base)->Repetitions(5);
