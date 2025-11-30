9ef951a4455123dea81c5c82b3cdbd61d31c8015
0KABE <RIN.OKAB3@GMAIL.COM>
2025-11-30 19:14:11 +0800

    parse test

--- test/parser_test.cpp
+++ test/parser_test.cpp
@@ -6,7 +6,25 @@
 
 #include <gtest/gtest.h>
 
-TEST(Parser, BasicParseOnce) {
+template <Entity (*f)(std::span<const char>&)>
+struct ParserFunc {
+  static Entity Parse(std::span<const char>& span) { return f(span); }
+};
+
+template <typename T>
+class ParserTest : public ::testing::Test {};
+
+using ParserImplementations = ::testing::Types<ParserFunc<ParseOnce_Base>,                                       //
+                                               ParserFunc<ParseOnce<FindFirstZeroByte_Base, ParseNumber_Base>>,  //
+                                               ParserFunc<ParseOnce<FindFirstZeroByte_Base, ParseNumber_SWAR_V1>>,
+                                               ParserFunc<ParseOnce<FindFirstZeroByte_Base, ParseNumber_SWAR_V2>>,
+                                               ParserFunc<ParseOnce<FindFirstZeroByte_SWAR, ParseNumber_Base>>,
+                                               ParserFunc<ParseOnce<FindFirstZeroByte_SWAR, ParseNumber_SWAR_V1>>,
+                                               ParserFunc<ParseOnce<FindFirstZeroByte_SWAR, ParseNumber_SWAR_V2>>>;
+
+TYPED_TEST_SUITE(ParserTest, ParserImplementations);
+
+TYPED_TEST(ParserTest, AllParsers) {
   using namespace std::string_view_literals;
 
   std::vector<std::tuple<std::span<const char>, Entity>> v{
@@ -39,87 +57,7 @@ TEST(Parser, BasicParseOnce) {
   };
 
   for (auto [span, expect] : v) {
-    const auto [name, temperature] = ParseOnce_Base(span);
-    EXPECT_EQ(name, expect.name);
-    EXPECT_EQ(temperature, expect.temperature);
-    EXPECT_TRUE(span.empty());
-  }
-}
-
-TEST(Parser, ParseOnceV1) {
-  using namespace std::string_view_literals;
-
-  std::vector<std::tuple<std::span<const char>, Entity>> v{
-      {"Dampier;34.3\n"sv, {"Dampier", 343}},            //
-      {"Kansas City;19.5\n"sv, {"Kansas City", 195}},    //
-      {"Benghazi;16.1\n"sv, {"Benghazi", 161}},          //
-      {"Portland (OR);8.8\n"sv, {"Portland (OR)", 88}},  //
-      {"Sokoto;17.8\n"sv, {"Sokoto", 178}},              //
-      {"Valletta;22.6\n"sv, {"Valletta", 226}},          //
-      {"Tokyo;15.0\n"sv, {"Tokyo", 150}},                //
-      {"Accra;30.0\n"sv, {"Accra", 300}},                //
-      {"Upington;36.3\n"sv, {"Upington", 363}},          //
-      {"Entebbe;23.8\n"sv, {"Entebbe", 238}},            //
-      {"Ljubljana;16.6\n"sv, {"Ljubljana", 166}},        //
-      {"New Orleans;8.3\n"sv, {"New Orleans", 83}},      //
-      {"Cracow;-1.7\n"sv, {"Cracow", -17}},              //
-      {"Brisbane;6.6\n"sv, {"Brisbane", 66}},            //
-      {"Hanoi;8.5\n"sv, {"Hanoi", 85}},                  //
-      {"Alexandra;8.0\n"sv, {"Alexandra", 80}},          //
-      {"Anadyr;-2.8\n"sv, {"Anadyr", -28}},              //
-      {"Denver;1.4\n"sv, {"Denver", 14}},                //
-      {"Blantyre;27.5\n"sv, {"Blantyre", 275}},          //
-      {"Singapore;53.3\n"sv, {"Singapore", 533}},        //
-      {"Thiès;33.4\n"sv, {"Thiès", 334}},                //
-      {"Hamburg;12.0\n"sv, {"Hamburg", 120}},            //
-      {"Bulawayo;8.9\n"sv, {"Bulawayo", 89}},            //
-      {"ASDGABASDGWTERYdfghonpn apsndonpgnpljkjlqwet7723lj =-=zxcvasd,.//sdaf!@#$%^&*()<>TERYdfghonpn apsndon;8.9\n"sv,
-       {"ASDGABASDGWTERYdfghonpn apsndonpgnpljkjlqwet7723lj =-=zxcvasd,.//sdaf!@#$%^&*()<>TERYdfghonpn apsndon",
-        89}},  //
-  };
-
-  for (auto [span, expect] : v) {
-    const auto [name, temperature] = ParseOnce_V1(span);
-    EXPECT_EQ(name, expect.name);
-    EXPECT_EQ(temperature, expect.temperature);
-    EXPECT_TRUE(span.empty());
-  }
-}
-
-TEST(Parser, ParseOnce) {
-  using namespace std::string_view_literals;
-
-  std::vector<std::tuple<std::span<const char>, Entity>> v{
-      {"Dampier;34.3\n"sv, {"Dampier", 343}},            //
-      {"Kansas City;19.5\n"sv, {"Kansas City", 195}},    //
-      {"Benghazi;16.1\n"sv, {"Benghazi", 161}},          //
-      {"Portland (OR);8.8\n"sv, {"Portland (OR)", 88}},  //
-      {"Sokoto;17.8\n"sv, {"Sokoto", 178}},              //
-      {"Valletta;22.6\n"sv, {"Valletta", 226}},          //
-      {"Tokyo;15.0\n"sv, {"Tokyo", 150}},                //
-      {"Accra;30.0\n"sv, {"Accra", 300}},                //
-      {"Upington;36.3\n"sv, {"Upington", 363}},          //
-      {"Entebbe;23.8\n"sv, {"Entebbe", 238}},            //
-      {"Ljubljana;16.6\n"sv, {"Ljubljana", 166}},        //
-      {"New Orleans;8.3\n"sv, {"New Orleans", 83}},      //
-      {"Cracow;-1.7\n"sv, {"Cracow", -17}},              //
-      {"Brisbane;6.6\n"sv, {"Brisbane", 66}},            //
-      {"Hanoi;8.5\n"sv, {"Hanoi", 85}},                  //
-      {"Alexandra;8.0\n"sv, {"Alexandra", 80}},          //
-      {"Anadyr;-2.8\n"sv, {"Anadyr", -28}},              //
-      {"Denver;1.4\n"sv, {"Denver", 14}},                //
-      {"Blantyre;27.5\n"sv, {"Blantyre", 275}},          //
-      {"Singapore;53.3\n"sv, {"Singapore", 533}},        //
-      {"Thiès;33.4\n"sv, {"Thiès", 334}},                //
-      {"Hamburg;12.0\n"sv, {"Hamburg", 120}},            //
-      {"Bulawayo;8.9\n"sv, {"Bulawayo", 89}},            //
-      {"ASDGABASDGWTERYdfghonpn apsndonpgnpljkjlqwet7723lj =-=zxcvasd,.//sdaf!@#$%^&*()<>TERYdfghonpn apsndon;8.9\n"sv,
-       {"ASDGABASDGWTERYdfghonpn apsndonpgnpljkjlqwet7723lj =-=zxcvasd,.//sdaf!@#$%^&*()<>TERYdfghonpn apsndon",
-        89}},  //
-  };
-
-  for (auto [span, expect] : v) {
-    const auto [name, temperature] = ParseOnce<FindFirstZeroByte_SWAR, ParseNumber_SWAR_V2>(span);
+    const auto [name, temperature] = TypeParam::Parse(span);
     EXPECT_EQ(name, expect.name);
     EXPECT_EQ(temperature, expect.temperature);
     EXPECT_TRUE(span.empty());
