9c1b523f2ce2f098ff0d5338657385c49453292b
0KABE <RIN.OKAB3@GMAIL.COM>
2025-12-12 03:18:04 +0800

    implement main

--- src/main.cpp
+++ src/main.cpp
@@ -2,13 +2,24 @@
 // Created by Chen, WenTao on 2025/11/2.
 //
 
+#include <fmt/base.h>
+
 #include <iostream>
 
-#include "data.h"
 #include "env.h"
 #include "file_memory_map.h"
-#include "file_reader.h"
+#include "parallelizer.h"
 
 class Reader {};
 
-int main() { std::cout << "main..." << std::endl; }
+int main() {
+  const FileMemoryMap file(Env::Instance().input_file);
+  Parallelizer parallelizer(Env::Instance().threads, 1 << 21, static_cast<std::span<const char>>(file));
+  const auto result = parallelizer.Process();
+
+  for (const auto& [name, statistics] : result) {
+    fmt::print("{}={}/{}/{} ", name, statistics.min / 10.0, statistics.max / 10.0,
+               statistics.sum / statistics.count / 10.0);
+  }
+  return 0;
+}
