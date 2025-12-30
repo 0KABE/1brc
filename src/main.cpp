57fe1125992d472e4abf962b82782ce883a20fcd
0KABE <RIN.OKAB3@GMAIL.COM>
2025-12-30 22:46:16 +0800

    use ranges instead

--- src/main.cpp
+++ src/main.cpp
@@ -3,6 +3,7 @@
 //
 
 #include <fmt/format.h>
+#include <fmt/ranges.h>
 
 #include <algorithm>
 
@@ -18,12 +19,12 @@ int main() {
   auto v = result | std::ranges::to<std::vector<StatisticsMap::KV>>();
   std::ranges::sort(v, {}, &StatisticsMap::KV::key);
 
-  fmt::print("{{");
-  for (const auto& [key, statistics] : v) {
-    fmt::print("{}={:.1f}/{:.1f}/{:.1f} ", key.name, statistics.min / 10.0,
-               static_cast<double>(statistics.sum) / statistics.count / 10.0, statistics.max / 10.0);
-  }
-  fmt::print("}}");
+  const auto& formated_results =
+      v | std::views::transform([](const StatisticsMap::KV& kv) {
+        return fmt::format("{}={:.1f}/{:.1f}/{:.1f}", kv.key.name, kv.value.min / 10.0,
+                           static_cast<double>(kv.value.sum) / kv.value.count / 10.0, kv.value.max / 10.0);
+      });
+  fmt::print("{{{}}}", fmt::join(formated_results, ", "));
 
   return 0;
 }
